e0f6e4359b2d0030be213a3563268dbd4938ead3
Andreea Cojocaru <andreea.cojocaru@cerner.com>
2020-05-14 15:46:08 +0300

    Added Redirect page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 import AboutPage from './AboutPage';
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { Route, Switch } from "react-router-dom";
 import HomePage from './HomePage';
 import PageNotFound from "./PageNotFound";
 
@@ -14,6 +14,7 @@ function App() {
             <Route path="/" exact component={HomePage} />
             <Route path="/courses" component={CoursesPage} />
             <Route path="/about" component={AboutPage} />
+            <Redirect from="/about-page" to="/about" />
             <Route component={PageNotFound} />
         </Switch>
     </div >;
